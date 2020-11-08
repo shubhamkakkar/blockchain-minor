a70a2e8b899247ffb4bf3c55c67f63c63599054b
Shubham Kakkar <shubhamkakkar@shubhams-MacBook-Pro.local>
2020-11-09 03:27:56 +0530

    shareBlock work in progres

--- src/resolvers/blockchainResolver/mutation/shareBlock/shareBlock.ts
+++ src/resolvers/blockchainResolver/mutation/shareBlock/shareBlock.ts
@@ -14,6 +14,8 @@ export default function shareBlock(
    * if true:
    *    * check if user exists
    *        if true: send the block to that user
+   *    for this in frontend we need a dropdown of users,
+   *    thus create a user route which returns all the user
    *     else: email user invite to the block
    * else: tell user the cipher ket is wrong
    * */
