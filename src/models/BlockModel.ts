526d4e942096d44c55f8cd235cd1b07fd5cca6d0
Shubham Kakkar <shubhamkakkar@shubhams-MacBook-Pro.local>
2020-11-08 19:35:45 +0530

    blockModel made

---
+++ src/models/BlockModel.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const BlockModel = new Schema(
+  {
+    data: {
+      type: String,
+      required: true,
+    },
+    prevHash: {
+      type: String,
+      required: true,
+    },
+    hash: {
+      type: String,
+      required: true,
+    },
+    timeStamp: {
+      type: Date,
+      required: true,
+    },
+    nounce: {
+      type: Number,
+      required: true,
+    },
+  },
+  { collection: 'Blockchain' },
+);
+export default model('BlockModel', BlockModel);
