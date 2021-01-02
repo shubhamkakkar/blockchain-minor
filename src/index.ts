4d89b0e0547b5cccae22f78396b1b2de9264cc15
shubham kakkar <shubham.kakkar98@gmail.com>
2021-01-03 03:50:00 +0530

    morgan added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
 
 import typeDefs from './typeDefs';
 import resolvers from './resolvers';
@@ -8,7 +9,8 @@ import context from './context';
 
 export default function app() {
   const expressApp = express();
-
+  if (process.env.NODE_ENV === 'development') expressApp.use(logger('dev'));
+  if (process.env.NODE_ENV === 'production') expressApp.use(logger('common'));
   expressApp.use(bodyParser.json());
   const server = new ApolloServer(
     {
