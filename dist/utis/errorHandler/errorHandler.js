987ca8395722f0beecf0690ad4437b34a97026b6
shubham kakkar <shubham.kakkar98@gmail.com>
2021-03-11 00:52:17 +0530

    dist removed

--- dist/utis/errorHandler/errorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_1 = require("graphql");
-function errorHandler(functionName, error) {
-    console.log(`InternalServerError ${functionName} e()`, error);
-    return new graphql_1.GraphQLError(`InternalServerError ${functionName} e()`);
-}
-exports.default = errorHandler;
