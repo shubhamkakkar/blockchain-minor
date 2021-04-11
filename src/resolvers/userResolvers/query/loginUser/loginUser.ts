356d3f00d8f79e092d4d3d0bfbe82c5934a570c4
shubham kakkar <shubham.kakkar98@gmail.com>
2021-04-12 01:46:20 +0530

    check for passowrd in login added

--- src/resolvers/userResolvers/query/loginUser/loginUser.ts
+++ src/resolvers/userResolvers/query/loginUser/loginUser.ts
@@ -2,9 +2,14 @@ import { GraphQLError } from 'graphql';
 
 import { ReturnedUser, TLoginArgs } from 'src/generated/graphql';
 import UserModel from 'src/models/UserModel';
+import { comparePasswordCrypt } from 'src/utis/bcrypt/bcrypt';
 import errorHandler from 'src/utis/errorHandler/errorHandler';
 import { generateToken } from 'src/utis/jwt/jwt';
 import ValidationContract from 'src/utis/validator/validator';
+
+interface GeneratedUser extends ReturnedUser {
+  password: string;
+}
 
 export default async function loginUser(args: TLoginArgs) {
   const contract = new ValidationContract();
@@ -22,12 +27,16 @@ export default async function loginUser(args: TLoginArgs) {
   try {
     const user = await UserModel.findOne({ email });
     if (user) {
-      const generatedUser = user.toObject() as ReturnedUser;
-      const token = generateToken(generatedUser._id);
-      return {
-        ...generatedUser,
-        token,
-      };
+      const generatedUser = user.toObject() as GeneratedUser;
+      const isPasswordCorrect = await comparePasswordCrypt(password, generatedUser.password);
+      if (isPasswordCorrect) {
+        const token = generateToken(generatedUser._id);
+        return {
+          ...generatedUser,
+          token,
+        };
+      }
+      return new GraphQLError('Password is incorrect');
     }
     return new GraphQLError('user does not exists');
   } catch (er) {
--- src/utis/bcrypt/bcrypt.ts
+++ src/utis/bcrypt/bcrypt.ts
@@ -2,8 +2,14 @@ import bcrypt from 'bcryptjs';
 
 const saltRounds = 10;
 
-export async function generatePasswordCrypt(password: string) {
+export function generatePasswordCrypt(password: string) {
   return bcrypt.hash(password, saltRounds)
     .then((hash) => hash)
-    .catch((er) => console.log('generatePasswordCrypt*() e', er));
+    .catch((er) => console.log('generatePasswordCrypt() e', er));
+}
+
+export function comparePasswordCrypt(password: string, hash: string) {
+  return bcrypt.compare(password, hash)
+    .then((compareResp) => compareResp)
+    .catch((er) => console.log('comparePasswordCrypt() e', er));
 }
